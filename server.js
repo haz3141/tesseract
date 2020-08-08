c2dfb5257ef6581670a10b32dd1bd58623659bb7
haz3141 <38766403+haz3141@users.noreply.github.com>
2020-08-07 22:04:57 -0400

    Setup basic Express server

---
+++ server.js
@@ -0,0 +1,17 @@
+// Package Imports
+const express = require("express");
+
+// Set Default Port
+const PORT = process.env.PORT || 3001;
+
+// Instantiate Express App
+const app = express();
+
+// Set Express Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Start Express Server
+app.listen(PORT, function () {
+  console.log(`🌎Server is running on: ${PORT}🌎`);
+});
